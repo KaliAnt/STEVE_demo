f977c455fa5080399db7d025ededad27ce0efb46
Calin Sulea <calin.g.sulea@gmail.com>
2018-05-12 01:40:57 +0300

    Added player controller and movement

---
+++ HackTMgame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float cameraSmooth;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, cameraSmooth);
+    }
+}
---
+++ HackTMgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rigidBody;
+    private Transform transform;
+    private Joystick joyStick;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        joyStick = FindObjectOfType<Joystick>();
+        transform = GetComponent<Transform>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float movementX = joyStick.Horizontal;
+        float movementY = joyStick.Vertical;
+
+ 
+        if (movementY!= 0 || movementX != 0)
+        {
+            rigidBody.velocity = new Vector2(movementX * speed, movementY * speed);
+        }
+    }
+}
