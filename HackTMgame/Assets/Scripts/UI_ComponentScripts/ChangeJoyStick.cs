dd51f1ebe339b3d52f1d1c2d6178287764b24758
Mathe Emese <emese.mathe.07@gmail.com>
2018-05-13 02:02:16 +0300

    Change options

---
+++ HackTMgame/Assets/Scripts/UI_ComponentScripts/ChangeJoyStick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeJoyStick : MonoBehaviour {
+
+	public Text text_;
+	public Button change_;
+	public GameObject joyStick_;
+	public GameObject rotate_;
+
+	bool isRight = true;
+	Transform transform_;
+
+
+	void Start () {
+		Button myBtn = change_.GetComponent<Button>();
+		myBtn.onClick.AddListener (TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		if (isRight)
+		{
+			text_.text = "Left hand";
+			isRight = !isRight;
+		
+			var tmp = rotate_.transform.position;
+			rotate_.transform.position = joyStick_.transform.position;
+			joyStick_.transform.position = tmp;
+			//change positions ...
+		} 
+		else 
+		{
+			text_.text = "Right hand";
+			isRight = !isRight;
+
+			var tmp = rotate_.transform.position;
+			rotate_.transform.position = joyStick_.transform.position;
+			joyStick_.transform.position = tmp;
+			//change positions
+		}
+	}
+}
