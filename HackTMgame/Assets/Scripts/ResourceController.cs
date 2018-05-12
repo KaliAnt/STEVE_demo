5bac3701723e6c14174dd587995e07ab90dca9f2
Calin Sulea <calin.g.sulea@gmail.com>
2018-05-12 14:49:20 +0300

    Added resource bar

---
+++ HackTMgame/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour {
+
+    public float maxCapacity;
+    public float currentCapacity;
+    public Image capacityBar;
+
+	// Use this for initialization
+	void Start () {
+
+        maxCapacity = 200;
+        currentCapacity = maxCapacity;
+        capacityBar.fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            mineResource(20.0f);
+        }
+	}
+
+    public float mineResource(float minePower)
+    {
+        float minedQuantity = 0;
+
+
+        if(currentCapacity <= minePower)
+        {
+            minedQuantity = currentCapacity;
+            Destroy(gameObject);
+        }
+        else{
+            currentCapacity -= minePower;
+        }
+
+        capacityBar.fillAmount = (maxCapacity - currentCapacity) / maxCapacity ; 
+        return minedQuantity;
+    }
+}
