4402d05c865589b8d86c0e5bfe6c31f6fec2d636
Mathe Emese <emese.mathe.07@gmail.com>
2018-05-12 19:38:44 +0300

    About added

---
+++ HackTMgame/Assets/Scripts/UI_ComponentScripts/About.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class About : MonoBehaviour {
+
+	public Button about_;
+	public GameObject panel_;
+	void Start ()
+	{
+		Button myBtn = about_.GetComponent<Button>();
+		myBtn.onClick.AddListener (TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		panel_.SetActive (true);			
+	}
+}
---
+++ HackTMgame/Assets/Scripts/UI_ComponentScripts/QuitAbout.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitAbout : MonoBehaviour {
+
+	public Button quit_;
+	public GameObject panel_;
+	void Start ()
+	{
+		Button myBtn = quit_.GetComponent<Button>();
+		myBtn.onClick.AddListener (TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		panel_.SetActive (false);			
+	}
+}
