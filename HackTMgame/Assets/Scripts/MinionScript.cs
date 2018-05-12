7a3b16d76d2468bbd5ec47a2c93085abceb38e93
Calin Sulea <calin.g.sulea@gmail.com>
2018-05-12 19:10:04 +0300

    Added metadata to gitignore

---
+++ HackTMgame/Assets/Scripts/MinionScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionScript : MonoBehaviour {
+
+    public GameObject ownerPlayer;
+    public float speed;
+    
+
+    public float destX, destY;
+    private Rigidbody2D minionBody;
+
+	// Use this for initialization
+	void Start () {
+        minionBody = GetComponent<Rigidbody2D>();
+	}
+	
+    public void setDestination(int x, int y)
+    {
+        destX = x;
+        destY = y;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+        while(transform.position.x != destX && transform.position.y != destY)
+        {
+            
+        }
+	}
+}
