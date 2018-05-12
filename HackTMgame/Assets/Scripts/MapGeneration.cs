24c04775ab3ed14982206706ba7b594e7c766e9a
Kali <kkytex@gmail.com>
2018-05-13 02:38:55 +0300

    Spawning planets

--- HackTMgame/Assets/Scripts/MapGeneration.cs
+++ HackTMgame/Assets/Scripts/MapGeneration.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class MapGeneration : MonoBehaviour {
 
+    public GameObject player;
     public GameObject res;
     public Color pink;
     public float mapWidth;
     public float mapHeight;
-
     public float spawnRate;
     public float rarity;
+
+    public GameObject planet;
+
+
+    private float planetSpawnRate = 10;
+    
     
     private Transform pinkT;
 	// Use this for initialization
@@ -25,9 +31,23 @@ public class MapGeneration : MonoBehaviour {
         int tiles = (int)((mapHeight * mapWidth * spawnRate / 100));
         int rate = (int) (tiles / ((mapHeight + mapWidth)/2));
 
+        int planets = 6; 
 
         for (int i = rate; i > 0; i--)
         {
+            if(planets > 0)
+            {
+                GameObject clonePlanet = (GameObject)Instantiate(planet);
+                float siz = Random.Range(0.3f, 0.7f);
+                SpriteRenderer planetColor = clonePlanet.transform.GetComponent<SpriteRenderer>();
+                planetColor.color = new Color32((byte)Random.Range(0x00, 0xFF), (byte)Random.Range(0x00, 0xFF), (byte)Random.Range(0x00, 0xFF), 0xFF);
+                clonePlanet.transform.localScale = new Vector3(siz, siz, 0);
+                
+                clonePlanet.transform.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 180.0f));
+               
+                clonePlanet.transform.position = new Vector3(Random.Range(-mapWidth + 30, mapWidth - 30), Random.Range(-mapHeight + 30, mapHeight - 30), 0);
+                planets--;
+            }
             GameObject cloneInstance = (GameObject)Instantiate(res);
 
             float size = Random.Range(0.3f, 0.7f);
@@ -41,6 +61,6 @@ public class MapGeneration : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
+        
 	}
 }
